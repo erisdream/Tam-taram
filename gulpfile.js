53d9ef269d77e87c4d741b6ed7bda0f53f82a073
erisdream <evgeniynedilko@mail.ru>
2017-10-24 15:34:19 +0300

    попытка интегрироваться с тревисом

--- gulpfile.js
+++ gulpfile.js
@@ -36,9 +36,11 @@ gulp.task('html', () => {
         .pipe(gulp.dest('./dist'));
 });
 
+
 gulp.watch('src/less/**/*.less', ['styles']);
 gulp.watch('src/**/*.html', ['html']);
 gulp.watch('src/img/**/*.*', ['img']);
 gulp.watch('src/js/**/*.*', ['js']);
 
 gulp.task('default', ['styles', 'html', 'img', 'js']);
+gulp.task('prod', ['styles', 'html', 'img', 'js']);
