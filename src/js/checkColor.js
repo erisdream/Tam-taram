fd3f92df7004f0eee8556b7fe340bc6a3560a2dd
erisdream <evgeniynedilko@mail.ru>
2017-11-02 22:08:19 +0300

    Correct the teacher's remarks

--- src/js/checkColor.js
+++ src/js/checkColor.js
@@ -1,16 +1,20 @@
 'use strict';
-const image = document.getElementById('image');
+const image = document.getElementById('productImage');
 document.addEventListener ('click', function (event) {
-    if(event.target.classList.contains('page-main__good-radio--color-white')) {
+   
+    if(event.target.getAttribute('value') == 'white') {
         image.src = 'img/tshirt_white.jpg';
     }
 
-    if(event.target.classList.contains('page-main__good-radio--color-yellow')) {
+    if(event.target.getAttribute('value')== 'yellow') {
         image.src = 'img/tshirt_yellow.jpg';
     }
 
-    if(event.target.classList.contains('page-main__good-radio--color-green')) {
+    if(event.target.getAttribute('value')== 'green') {
         image.src = 'img/tshirt_green.jpg';
     }
 
 });
+
+
+
